fd35097e93088fa075568d13472c39565fb2d9d5
Oladokun Lukman Tunde <lukmanoladokun86@gmail.com>
2022-04-23 08:50:16 +0100

    Added form.css

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import "./Form.css";
 
 export class Form extends Component {
     constructor(props){
         super(props);
-        this.state = { fnameValue: "", lnameValue: "", emailValue: "", selVal: "Male", adresValue: "", bioValue: "" };
+        
+        this.state = { 
+          fnameValue: "", 
+          lnameValue: "", 
+          emailValue: "", 
+          selVal: "Select gender", 
+          adresValue: "", 
+          bioValue: "" 
+        };
      
     }
 
@@ -43,52 +52,100 @@ export class Form extends Component {
   render() {
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            <div>First name</div>
-            <input type="text" name="fnameValue" id="fname" value={this.state.fname} onChange={this.handleFnameChange}/>
+        <form onSubmit={this.handleSubmit} className="form">
+          <label className="d-flex">
+            <div >First name</div>
+            <input 
+              type="text" 
+              name="fnameValue" 
+              id="fname" 
+              value={this.state.fname} 
+              onChange={this.handleFnameChange}
+            />
           </label>
 
-          <label>
+          <label className="d-flex">
             <div>Last name</div>
-            <input type="text" name="lname" id="lname" value={this.state.lname} onChange={this.handleLnameChange}/>
+            <input 
+              type="text" 
+              name="lname" 
+              id="lname" 
+              value={this.state.lname} 
+              onChange={this.handleLnameChange}
+            />
           </label>
 
-          <label>
+          <label className="d-flex">
             <div>Email</div>
-            <input type="email" name="email" id="email" value={this.state.email} onChange={this.handleEmailChange}/>
+            <input 
+              type="email" 
+              name="email" 
+              id="email" 
+              value={this.state.email} 
+              onChange={this.handleEmailChange}
+            />
           </label>
 
-          <label>
+          <label className="d-flex">
             <div>Gender</div>
             <select value={this.state.selVal} onChange={this.handleSelect}>
-              <option value="Gender">Gender</option>
+              <option value="Gender">Select gender</option>
               <option value="Male">Male</option>
               <option value="Female">Female</option>
             </select>
           </label>
 
-           <label>
+           <label className="d-flex">
             <div>Address</div>
-            <textarea name="adresValue" id="adresname" value= {this.state.adresValue} onChange= {this.handleAdresChange} />
+            <textarea 
+              name="adresValue" 
+              id="adresname"
+              rows= "3" 
+              value= {this.state.adresValue} 
+              onChange= {this.handleAdresChange} 
+            />
           </label>
 
-          <label>
+          <label className="d-flex">
             <div>Bio</div>
-            <textarea name="name" id="bioname" value= {this.state.bioValue} onChange= {this.handleBioChange} />
+            <textarea 
+              name="name" 
+              id="bioname"
+              rows="3" 
+              value= {this.state.bioValue} 
+              onChange= {this.handleBioChange} 
+            />
           </label>
         
           <div>
-            <button type="submit" >Submit</button>
+            <button type="submit" className="btn" >Submit</button>
           </div>
         </form>
 
-        <p> First name: { this.state.fnameValue} </p>
-        <p> Last name: {this.state.lnameValue} </p>
-        <p> Email address: {this.state.emailValue} </p>
-        <p> Gender: {this.state.selVal} </p>
-        <p> Address: {this.state.adresValue } </p>
-        <p> Bio: {this.state.bioValue} </p> 
+        <div className="card">
+          <div className="d-flex">
+            <div className="title">Full name: </div>
+            <div className="user-input"> { this.state.fnameValue} {this.state.lnameValue} </div>
+          </div>
+          
+          <div className="d-flex">
+            <div className="title">Email</div>
+            <div className="user-input"> {this.state.emailValue} </div>
+          </div>
+          <div className="d-flex">
+            <div className="title">Gender</div>
+            <div className="user-input"> {this.state.selVal} </div>
+          </div>
+          <div className="d-flex">
+            <div className="title">Address</div>
+            <div className="user-input"> { this.state.adresValue }</div>
+          </div>
+          <div className="d-flex">
+            <div className="title">Bio</div>
+            <div className="user-input">{this.state.bioValue}</div>
+          </div>
+        </div>
+        
       </>
     );
   }
